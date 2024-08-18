95b8ea27b61b65c650653d43cc1ad3e0a9709eea
Ammar Daham <ammar.daham@metropolia.fi>
2024-08-18 13:48:04 +0300

    rename to sliceshop

--- ecommerce-backend/src/main/java/com/sliceshop/SliceShopServlet.java
+++ ecommerce-backend/src/main/java/com/sliceshop/SliceShopServlet.java
@@ -1,16 +1,24 @@
-package com.ecommerce;
+package com.sliceshop;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class EcommerceServlet extends HttpServlet
+public class SliceShopServlet extends HttpServlet
 {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException
+	{
+		super.init(config);
+		System.out.println("testing ..");
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
